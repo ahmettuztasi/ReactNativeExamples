6120f2a767d7c141e58a01681562fd5f2cb94f26
tuztasiahmet <tuztasiahmet@gmail.com>
2020-04-09 14:51:46 +0300

    added navigation options example

--- Examples/StackNavigator.js
+++ Examples/StackNavigator.js
@@ -1,18 +1,31 @@
 //npm install react-navigation-stack
-import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 import IconExample from './IconExample';
 import FlatListExample from './FlatListExample';
 
 const screens = {
     IconExample: {
-        screen: IconExample
+        screen: IconExample,
+        navigationOptions: {
+            title: 'Icon Example',
+            headerStyle: { backgroundColor: '#333' }
+        }
     },
     FlatListExample: {
-        screen: FlatListExample
+        screen: FlatListExample,
+        navigationOptions: {
+            title: 'Flatlist Example',
+            headerStyle: { backgroundColor: '#eee' }
+        }
     }
 }
 
-const StackNavigator = createStackNavigator(screens);
+const StackNavigator = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#eee', height: 60 }
+    }
+});
 
 export default createAppContainer(StackNavigator);
